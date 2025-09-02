4ce14015b92be4521a200641de0ef2c90cefe1d0
Omar Elsawi <omarelsawi98@gmail.com>
2025-09-02 12:33:22 +0300

    Refactor routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -7,20 +7,58 @@ import {
 } from "../controllers/booksController.js";
 import {
   checkoutBook,
-  getOverdueBooks
+  getOverdueBooks,
 } from "../controllers/borrowerCheckoutController.js";
 import parseJsonBody from "../utils/parser.js";
 
 const bookRoutes = async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const urlSegments = req.url.split("/");
-  const id = urlSegments.length > 2 ? urlSegments[2] : null;
+  const secondSegment = urlSegments.length > 2 ? urlSegments[2] : null;
   let body = {};
   const searchParams = url.searchParams.get("search");
-  switch (req.method) {
-    case "GET":
-      res.setHeader("Content-Type", "application/json");
-      if (urlSegments[2] === 'overdue'){
+  res.setHeader("Content-Type", "application/json");
+
+  // Route: /books/:id/checkout
+  if (!isNaN(secondSegment) && urlSegments[3] === "checkout") {
+    switch (req.method) {
+      case "POST":
+        try {
+          body = await parseJsonBody(req);
+          if(!body.borrowerId){
+            res.statusCode = 400
+            throw new Error('Borrower ID is required')
+          }
+          if(!body.dueDate){
+            res.statusCode = 400
+            throw new Error('Due Date is required')
+          }
+          const queryResponse = await checkoutBook(
+            body.borrowerId,
+            secondSegment,
+            body.dueDate
+          );
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          if(!res.statusCode) res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
+        break;
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
+        break;
+    }
+  }
+  // Route: /books/overdue
+  else if (secondSegment === "overdue") {
+    switch (req.method) {
+      case "GET":
         try {
           const queryResponse = await getOverdueBooks();
           res.statusCode = 200;
@@ -28,48 +66,80 @@ const bookRoutes = async (req, res) => {
         } catch (err) {
           res.statusCode = 400;
           res.end(JSON.stringify({ error: err.message }));
-        } finally {
-          break;
         }
-      } else if (!searchParams) {
-        try {
-          const queryResponse = await getBooks();
-          res.statusCode = 200;
-          res.end(JSON.stringify(queryResponse));
-        } catch (err) {
-          res.statusCode = 400;
-          res.end(JSON.stringify({ error: err.message }));
-        } finally {
-          break;
-        }
-      } else {
-        try {
-          const queryResponse = await searchBook(searchParams);
-          res.statusCode = 200;
-          res.end(JSON.stringify(queryResponse));
-        } catch (err) {
-          res.statusCode = 400;
-          res.end(JSON.stringify({ error: err.message }));
-        } finally {
-          break;
-        }
-      }
-    case "POST":
-      res.setHeader("Content-Type", "application/json");
-      console.log(urlSegments);
-      if (urlSegments[3] === "checkout") {
+        break;
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
+        break;
+    }
+  }
+  // Route: /books/:id
+  else if (!isNaN(secondSegment)) {
+    switch (req.method) {
+      case "PUT":
         try {
           body = await parseJsonBody(req);
-          const queryResponse = await checkoutBook(body.borrowerId, id, body.dueDate);
+          const queryResponse = await updateBook(secondSegment, body);
+          if (!queryResponse) throw new Error("Book not found");
           res.statusCode = 200;
           res.end(JSON.stringify(queryResponse));
         } catch (err) {
           res.statusCode = 400;
           res.end(JSON.stringify({ error: err.message }));
-        } finally {
-          break;
         }
-      } else {
+        break;
+      case "DELETE":
+        try {
+          if (isNaN(secondSegment)) throw new Error("ID must be a number");
+          const queryResponse = await deleteBook(secondSegment);
+          if (!queryResponse) throw new Error("Book not found");
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
+        break;
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
+        break;
+    }
+  }
+  // Route: /books
+  else {
+    switch (req.method) {
+      case "GET":
+        if (searchParams) {
+          try {
+            const queryResponse = await searchBook(searchParams);
+            res.statusCode = 200;
+            res.end(JSON.stringify(queryResponse));
+          } catch (err) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({ error: err.message }));
+          }
+        } else {
+          try {
+            const queryResponse = await getBooks();
+            res.statusCode = 200;
+            res.end(JSON.stringify(queryResponse));
+          } catch (err) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({ error: err.message }));
+          }
+        }
+        break;
+      case "POST":
         try {
           body = await parseJsonBody(req);
           const queryResponse = await addBook(
@@ -84,41 +154,17 @@ const bookRoutes = async (req, res) => {
         } catch (err) {
           res.statusCode = 400;
           res.end(JSON.stringify({ error: err.message }));
-        } finally {
-          break;
         }
-      }
-    case "PUT":
-      res.setHeader("Content-Type", "application/json");
-      try {
-        if (!id) throw new Error("ID needs to be specified");
-        if (isNaN(id)) throw new Error("ID must be a number");
-        body = await parseJsonBody(req);
-        const queryResponse = await updateBook(id, body);
-        if (!queryResponse) throw new Error("Book not found");
-        res.statusCode = 200;
-        res.end(JSON.stringify(queryResponse));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: err.message }));
-      } finally {
         break;
-      }
-    case "DELETE":
-      res.setHeader("Content-Type", "application/json");
-      try {
-        if (!id) throw new Error("ID needs to be specified");
-        if (isNaN(id)) throw new Error("ID must be a number");
-        const queryResponse = await deleteBook(id);
-        if (!queryResponse) throw new Error("Book not found");
-        res.statusCode = 200;
-        res.end(JSON.stringify(queryResponse));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: err.message }));
-      } finally {
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
         break;
-      }
+    }
   }
 };
 
--- src/routes/borrowerRoutes.js
+++ src/routes/borrowerRoutes.js
@@ -6,7 +6,7 @@ import {
 } from "../controllers/borrowersController.js";
 import {
   returnBook,
-  getBorrowersBooks
+  getBorrowersBooks,
 } from "../controllers/borrowerCheckoutController.js";
 import parseJsonBody from "../utils/parser.js";
 
@@ -14,94 +14,130 @@ const borrowerRoutes = async (req, res) => {
   const urlSegments = req.url.split("/");
   const id = urlSegments.length > 2 ? urlSegments[2] : null;
   let body = {};
-  switch (req.method) {
-    case "GET":
-      if (!isNaN(id)) {
-        if (urlSegments[3] === "listBooks") {
-          res.setHeader("Content-Type", "application/json");
-          try {
-            const queryResponse = await getBorrowersBooks(id);
-            res.statusCode = 200;
-            res.end(JSON.stringify(queryResponse));
-          } catch (err) {
-            res.statusCode = 400;
-            res.end(JSON.stringify({ error: err.message }));
-          } finally {
-            break;
-          }
-        }
-      }
-      res.setHeader("Content-Type", "application/json");
-      try{
-        const queryResponse = await getBorrowers();
-        res.statusCode = 200;
-        res.end(JSON.stringify(queryResponse));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: err.message }));
-      } finally {break;}
+  res.setHeader("Content-Type", "application/json");
 
-    case "POST":
-        if (urlSegments[3] === "return") {
-          try {
-            body = await parseJsonBody(req);
-            if (!body.checkoutId) throw new Error("CheckoutId is required");
-            const queryResponse = await returnBook(body.checkoutId, id);
-            if (!queryResponse)
-              throw new Error("This checkout ID doesn't exist");
-            res.statusCode = 200;
-            res.end(JSON.stringify(queryResponse));
-          } catch (err) {
-            console.log(err.statusCode);
-            res.statusCode = 400;
-            res.end(JSON.stringify({ error: err.message }));
-          } finally {
-            break;
-          }
+  // Route: /borrowers/:id/listBooks
+  if (id && urlSegments[3] === "listBooks") {
+    switch (req.method) {
+      case "GET":
+        try {
+          if (isNaN(id)) throw new Error("ID must be a number");
+          const queryResponse = await getBorrowersBooks(id);
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
         }
-      res.setHeader("Content-Type", "application/json");
-      try {
-        body = await parseJsonBody(req);
-        const queryResponse = await addBorrower(body.name, body.email);
-        res.statusCode = 200;
-        res.end(JSON.stringify(queryResponse));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: err.message }));
-      } finally {
         break;
-      }
-    case "PUT":
-      res.setHeader("Content-Type", "application/json");
-      try {
-        if (!id) throw new Error("Id needs to be specified");
-        if (isNaN(id)) throw new Error("Id must be a number");
-        body = await parseJsonBody(req);
-        const queryResponse = await updateBorrower(id, body);
-        if (!queryResponse) throw new Error("Borrower not found");
-        res.statusCode = 200;
-        res.end(JSON.stringify(queryResponse));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: err.message }));
-      } finally {
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
         break;
-      }
-    case "DELETE":
-      res.setHeader("Content-Type", "application/json");
-      try {
-        if (!id) throw new Error("Id needs to be specified");
-        if (isNaN(id)) throw new Error("Id must be a number");
-        const queryResponse = await deleteBorrower(id);
-        if (!queryResponse) throw new Error("Borrower not found");
-        res.statusCode = 200;
-        res.end(JSON.stringify(queryResponse));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: err.message }));
-      } finally {
+    }
+  }
+  // Route: /borrowers/:id/return
+  else if (id && urlSegments[3] === "return") {
+    switch (req.method) {
+      case "POST":
+        try {
+          if (isNaN(id)) throw new Error("ID must be a number");
+          body = await parseJsonBody(req);
+          if (!body.checkoutId) throw new Error("CheckoutId is required");
+          const queryResponse = await returnBook(body.checkoutId, id);
+          if (!queryResponse) throw new Error("This checkout ID doesn't exist");
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
         break;
-      }
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
+        break;
+    }
+  }
+  // Route: /borrowers/:id
+  else if (id) {
+    switch (req.method) {
+      case "PUT":
+        try {
+          if (isNaN(id)) throw new Error("Id must be a number");
+          body = await parseJsonBody(req);
+          const queryResponse = await updateBorrower(id, body);
+          if (!queryResponse) throw new Error("Borrower not found");
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
+        break;
+      case "DELETE":
+        try {
+          if (isNaN(id)) throw new Error("Id must be a number");
+          const queryResponse = await deleteBorrower(id);
+          if (!queryResponse) throw new Error("Borrower not found");
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
+        break;
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
+        break;
+    }
+  }
+  // Route: /borrowers
+  else {
+    switch (req.method) {
+      case "GET":
+        try {
+          const queryResponse = await getBorrowers();
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
+        break;
+      case "POST":
+        try {
+          body = await parseJsonBody(req);
+          const queryResponse = await addBorrower(body.name, body.email);
+          res.statusCode = 200;
+          res.end(JSON.stringify(queryResponse));
+        } catch (err) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: err.message }));
+        }
+        break;
+      default:
+        res.statusCode = 405;
+        res.end(
+          JSON.stringify({
+            error: `Method ${req.method} not allowed for this route`,
+          })
+        );
+        break;
+    }
   }
 };
 
